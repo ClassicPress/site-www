84c8c9698906b70d6f514e82b3a68e092542b8d9
James Nylen <jnylen@gmail.com>
2020-01-22 18:00:34 +0000

    Rewrite latest-github-release plugin with reusable code

--- wp-content/plugins/latest-github-release/latest-github-release.php
+++ wp-content/plugins/latest-github-release/latest-github-release.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Latest Github Release
  * Description: Automatically add a download link to the latest Github repo release zips with a shortcode like [latest_github_release user="github" repo="hub"]
- * Version: 1.2.0
+ * Version: 2.0.0
  * Author: Laurence Bahiirwa, James Nylen
  * Author URI: https://omukiguy.com
  * Plugin URI: https://github.com/bahiirwa/latest-github-release
@@ -27,10 +27,11 @@ class LatestGithubRelease {
 	 * Add action to process shortcodes.
 	 *
 	 * @since 0.1.0
+	 * @since 2.0.0 The function is now static.
 	 *
 	 */
-	public function register() {
-		add_shortcode( 'latest_github_release', [ $this, 'process_shortcode' ] );
+	public static function register() {
+		add_shortcode( 'latest_github_release', [ __CLASS__, 'process_shortcode' ] );
 	}
 
 	/**
@@ -39,17 +40,17 @@ class LatestGithubRelease {
 	 * This public function processes the cp_release_link shortcode into HTML markup.
 	 *
 	 * @since 0.1.0
+	 * @since 2.0.0 The function is now static.
 	 *
 	 * @param array $atts Shortcode arguments.
 	 * @return string <a href="(zip url)" class="cp-release-link">$atts[name]</a>
 	 */
-	public function process_shortcode( $atts ) {
+	public static function process_shortcode( $atts ) {
 		// Default values for when not passed in shortcode.
 		$defaults = [
-			'user' => '',
-			'repo' => '',
-			// set default button name to Download
-			'name' => 'Download Zip',
+			'user'  => '',
+			'repo'  => '',
+			'name'  => 'Download Zip', // default button name
 			'class' => 'latest-github-release-link',
 		];
 
@@ -62,27 +63,26 @@ class LatestGithubRelease {
 
 		// Validate the user and the repo.
 		if ( empty( $atts['user'] ) || empty( $atts['repo'] ) ) {
-			return '<!-- [latest_github_release] missing user or repo! -->';
+			return '<!-- [latest_github_release] Missing user or repo! -->';
 		}
 
-		// Get any existing copy of our transient data
-		$zip_url = get_transient( $this->get_transient_name( $atts ) );
+		// Get the release data from GitHub.
+		$release_data = self::get_release_data_cached( $atts );
+		if ( is_wp_error( $release_data ) ) {
+			return (
+				'<!-- [latest_github_release] '
+				. esc_html( $release_data->get_error_message() )
+				. ' -->'
+			);
+		}
 
-		if ( empty( $zip_url ) ) {
-			$zip_url = $this->get_release_zip_url( $atts );
-
-			if ( empty( $zip_url ) ) {
-				// An error occurred, and the `get_release_zip_url()` function
-				// should have already echoed an appropriate message inside a
-				// comment.
-				return '';
-			}
-
-			// Save zip link in transient inside DB to reduce network calls.
-			set_transient(
-				$this->get_transient_name( $atts ),
-				$zip_url,
-				15 * MINUTE_IN_SECONDS
+		// Get the URL to a release zipfile.
+		$zip_url = self::get_zip_url_for_release( $release_data );
+		if ( is_wp_error( $zip_url ) ) {
+			return (
+				'<!-- [latest_github_release] '
+				. esc_html( $zip_url->get_error_message() )
+				. ' -->'
 			);
 		}
 
@@ -96,7 +96,7 @@ class LatestGithubRelease {
 		/**
 		 * Filters the HTML for the release link.
 		 *
-		 * @since 1.3.0
+		 * @since 2.0.0
 		 *
 		 * @param string $html The link HTML.
 		 * @param array  $atts The full array of shortcode attributes.
@@ -105,14 +105,65 @@ class LatestGithubRelease {
 	}
 
 	/**
-	 * Call out to the GitHub API to get a release zip URL for the given repository.
+	 * Fetch release data from GitHub or return it from a cached value.
 	 *
-	 * @since 1.2.0
+	 * @since 2.0.0
 	 *
 	 * @param array $atts Array containing 'user' and 'repo' arguments.
-	 * @return string URL of latest zip release file on GitHub.
+	 * @return array|\WP_Error Release data from GitHub, or an error object.
 	 */
-	public function get_release_zip_url( $atts ) {
+	public static function get_release_data_cached( $atts ) {
+		// Get any existing copy of our transient data
+		$release_data = get_transient( self::get_transient_name( $atts ) );
+
+		if ( empty( $release_data ) ) {
+			$release_data = self::get_release_data( $atts );
+
+			if ( is_wp_error( $release_data ) ) {
+				return $release_data;
+			}
+
+			// Save release data in transient inside DB to reduce network calls.
+			set_transient(
+				self::get_transient_name( $atts ),
+				$release_data,
+				15 * MINUTE_IN_SECONDS
+			);
+		}
+
+		return $release_data;
+	}
+
+	/**
+	 * Return the name of the transient that should be used to cache the
+	 * release information for a repository.
+	 *
+	 * @since 1.2.0
+	 * @since 2.0.0 The function is now static, and the transient names have
+	 * changed because the full release data is stored instead of just the URL
+	 * to a zip file.
+	 *
+	 * @param array $atts Array containing 'user' and 'repo' arguments.
+	 * @return string Transient name to use for caching this repository.
+	 */
+	public static function get_transient_name( $atts ) {
+		return (
+			'lgr_api_'
+			. substr( md5( $atts['user'] . '/' . $atts['repo'] ), 0, 16 )
+		);
+	}
+
+	/**
+	 * Fetch release data from GitHub.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @internal - use self::get_release_data_cached() instead.
+	 *
+	 * @param array $atts Array containing 'user' and 'repo' arguments.
+	 * @return array|\WP_Error Release data from GitHub, or an error object.
+	 */
+	private static function get_release_data( $atts ) {
 		// Build the GitHub API URL for the latest release.
 		$api_url = (
 			'https://api.github.com/repos/'
@@ -122,52 +173,63 @@ class LatestGithubRelease {
 
 		// Make API call.
 		$response = wp_remote_get( esc_url_raw( $api_url ) );
-		// Error catch for failed API call.
+
 		if ( is_wp_error( $response ) ) {
-			echo '<!-- [latest_github_release] error: ';
-			echo esc_html( $response->get_error_message() );
-			echo ' -->';
-			return null;
+			return $response;
 		}
 
 		// Parse the JSON response from the API into an array of data.
-		$api_response = json_decode( wp_remote_retrieve_body( $response ), true );
+		$response_body = wp_remote_retrieve_body( $response );
+		$response_json = json_decode( $response_body, true );
+		$status_code = wp_remote_retrieve_response_code( $response );
 
-		// If any files were uploaded for this release, use the first one.
-		// TODO: Allow specifying which file to use somehow (name regex?)
-		if ( ! empty( $api_response['assets'] ) ) {
-			return $api_response['assets'][0]['browser_download_url'];
+		if ( empty( $response_json ) || $status_code !== 200 ) {
+			return new \WP_Error(
+				'invalid_data',
+				'Invalid data returned from GitHub',
+				[
+					'code' => $status_code,
+					'body' => empty( $response_json ) ? $response_body : $response_json,
+				]
+			);
 		}
 
-		// Otherwise, build a URL based on the tag name of the latest release.
-		$version = $api_response['tag_name'];
-
-		if ( empty( $version ) ) {
-			echo '<!-- [latest_github_release] no releases found! -->';
-			return null;
-		}
-
-		return (
-			'https://github.com/'
-			. $atts['user'] . '/' . $atts['repo']
-			. '/archive/' . $version . '.zip'
-		);
+		return $response_json;
 	}
 
 	/**
-	 * Return the name of the transient that should be used to cache the
-	 * release information for a repository.
+	 * Given a set of release data from the GitHub API, return a release zip URL.
 	 *
-	 * @since 1.2.0
+	 * @since 2.0.0
 	 *
-	 * @param array $atts Array containing 'user' and 'repo' arguments.
-	 * @return string Transient name to use for caching this repository.
+	 * @param array $release_data Release data from the GitHub API.
+	 * @return string URL of latest zip release file on GitHub.
 	 */
-	public function get_transient_name( $atts ) {
-		return 'lgr_' . substr( md5( $atts['user'] . '/' . $atts['repo'] ), 0, 16 );
+	public static function get_zip_url_for_release( $release_data ) {
+		// If any files were uploaded for this release, use the first one.
+		// TODO: Allow specifying which file to use somehow (name regex?)
+		if ( ! empty( $release_data['assets'] ) ) {
+			return $release_data['assets'][0]['browser_download_url'];
+		}
+
+		// Otherwise, build a URL based on the tag name of the latest release.
+		$version = $release_data['tag_name'];
+
+		// Extract the user and repo name from the GitHub API URL.
+		preg_match(
+			'#^https://api\.github\.com/repos/([^/]+)/([^/]+)/releases/#',
+			$release_data['url'],
+			$matches
+		);
+		$user = $matches[1];
+		$repo = $matches[2];
+
+		return (
+			'https://github.com/'
+			. $user . '/' . $repo
+			. '/archive/' . $version . '.zip'
+		);
 	}
 }
 
-// On Activation. Start the Plugin class.
-$CP_release_link = new LatestGithubRelease;
-$CP_release_link->register();
+LatestGithubRelease::register();
