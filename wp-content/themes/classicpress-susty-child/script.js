c5d89266e6d7036b4905822d6effdd29f09b8b35
James Nylen <jnylen@gmail.com>
2020-03-02 00:34:01 +0000

    Some fixes for IE11 and older Edge:
    - Make submenus expand correctly on hover (via JS)
    - Make submenus look right
    - Make submenu keyboard navigation work (via JS)
    - Fix sidebar width (via JS)
    
    See: https://forums.classicpress.net/t/dropdown-not-working-on-edge/2187

--- wp-content/themes/classicpress-susty-child/script.js
+++ wp-content/themes/classicpress-susty-child/script.js
@@ -20,4 +20,27 @@ jQuery( document ).ready( function( $ ) {
 
 		return false; // do not follow link destination
 	} );
+
+	// Fix main menu hover and keyboard navigation on IE11 and older Edge
+	// Note, also using lack of :focus-within as a proxy for whatever issue
+	// causes submenus to not expand normally (broken selector specificity?)
+	// and whatever issue causes the sidebar to display incorrectly (width too
+	// narrow)
+	try {
+		document.querySelector( ':focus-within' );
+	} catch ( e ) {
+		var sel = '.nav--toggle-sub li';
+		function submenuExpand() {
+			$( this ).children( 'ul.sub-menu' ).addClass( 'open' );
+		}
+		function submenuCollapse() {
+			$( this ).children( 'ul.sub-menu' ).removeClass( 'open' );
+		}
+		$( document )
+			.on( 'mouseover', sel, submenuExpand )
+			.on( 'mouseout', sel, submenuCollapse )
+			.on( 'focusin', sel, submenuExpand )
+			.on( 'focusout', sel, submenuCollapse );
+		$( document.body ).addClass( 'ie11' );
+	}
 } );
