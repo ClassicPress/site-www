08f6d95cfb086773821796259b3670b11b494ae1
Tim Hughes <timbocode@users.noreply.github.com>
2020-09-15 03:55:42 +0100

    Update blog category pages to show only excerpt (#19)
    And prepare to remove the classicpress-blog category from the website

--- wp-content/themes/classicpress-susty-child/archive.php
+++ wp-content/themes/classicpress-susty-child/archive.php
@@ -9,36 +9,27 @@
 
 get_header();
 ?>
-
 	<div id="primary">
 		<main id="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php
+		if ( have_posts() ) : ?>
 
-			<header>
-				<?php
-				the_archive_title( '<h3>', '</h3>' );
-				the_archive_description( '<div class="archive-desc">', '</div>' );
-				?>
-			</header>
+			<div class="blog-list"> <?php
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+					get_template_part( 'template-parts/content-blog', get_post_type() );
 
-			endwhile;
+				endwhile; ?>
+
+			</div><!-- .blog-list --> <?php
 
 			the_posts_navigation();
 
-		else :
+		else:
 
 			get_template_part( 'template-parts/content', 'none' );
 
