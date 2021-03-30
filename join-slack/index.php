9de1031c7f61f07f420225a41741a54729785174
James Nylen <jnylen@gmail.com>
2021-03-30 18:34:38 +0000

    Update Slack invite

--- join-slack/index.php
+++ join-slack/index.php
@@ -1,4 +1,4 @@
 <?php
 
 header('HTTP/1.1 302 Found');
-header('Location: https://join.slack.com/t/classicpress/shared_invite/zt-ia3a5fzm-FpLt8n4OArPwbjdEs6WZBQ');
+header('Location: https://join.slack.com/t/classicpress/shared_invite/zt-ohsplfhg-YY2iXhKyh6REUSRTNIe7Lg');
